1d12b06e25519d763ce96050ec14bd4ad51a0cec
camilo <kamiloandres200518@gmail.com>
2024-05-09 17:59:15 -0500

    backend

---
+++ Back.end/Shoe-StoreCamilo/src/main/java/com/example/Shoe/StoreCamilo/ShoeStoreCamiloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shoe.StoreCamilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeStoreCamiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeStoreCamiloApplication.class, args);
+	}
+
+}
---
+++ Back.end/Shoe-StoreCamilo/src/test/java/com/example/Shoe/StoreCamilo/ShoeStoreCamiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shoe.StoreCamilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStoreCamiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
